d83fa20b877e729d7cf6793bd7cdb85ec5a0aecf
gvilaro <59828636+gvilaro@users.noreply.github.com>
2021-05-04 15:57:47 +0200

    todo 4

--- notes/src/main/java/cat/tecnocampus/notes/usersAdapters/userExistsAdapter.java
+++ notes/src/main/java/cat/tecnocampus/notes/usersAdapters/userExistsAdapter.java
@@ -2,6 +2,7 @@ package cat.tecnocampus.notes.usersAdapters;
 
 import cat.tecnocampus.notes.application.portsOut.CallUserExists;
 import cat.tecnocampus.notes.domain.Note;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Component;
@@ -11,17 +12,20 @@ import org.springframework.web.client.RestTemplate;
 public class userExistsAdapter implements CallUserExists {
     private RestTemplate restTemplate;
     private CircuitBreakerFactory circuitBreakerFactory;
+    private String usersServiceUrl;
 
-    public userExistsAdapter(RestTemplate restTemplate, CircuitBreakerFactory circuitBreakerFactory) {
+    public userExistsAdapter(RestTemplate restTemplate, CircuitBreakerFactory circuitBreakerFactory,
+                             @Value("${app.users-service.host}") String usersServiceUrl) {
         this.restTemplate = restTemplate;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.usersServiceUrl = "http://" + usersServiceUrl + "users/exists/";
     }
 
     @Override
     public String sendNote(Note note) {
         CircuitBreaker circuitBreaker = circuitBreakerFactory.create("users");
         return circuitBreaker.run(
-                () -> restTemplate.getForObject("http://localhost:8081/users/exists/" + note.getUserName(), String.class),
+                () -> restTemplate.getForObject(usersServiceUrl + note.getUserName(), String.class),
                 throwable -> {
                     System.out.println(throwable.getMessage());
                     return  "opened";});
